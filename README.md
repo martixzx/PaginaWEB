2e5ceb7df64c484ac0cf767fefb684208940d613
Stefano Martin <see.martiinez@gmail.com>
2021-10-19 03:51:14 +0200

    actualizacion juego con escenario seleccion de personajes

---
+++ README.md
@@ -0,0 +1,5 @@
+Personal WEB
+
+www.stefanomartin.es
+
+Proximamente
---
+++ javascript.js
@@ -0,0 +1,142 @@
+let inicio = document.getElementById("inicio")
+let seleccion= document.getElementById("seleccion")
+function empezar(){
+    
+    inicio.style.display = "none"
+    seleccion.style.display= "flex"
+}
+
+// let nodo.vida.jugador1= (parrafo=div>parrafo) // barra de vida del jugador1 
+// let nodo.vida.jugador2=(parrafo=div>parrafo) // barra de vida del jugador 2 
+
+function atacar (personaje){
+    
+    switch (personaje) {
+        case 'jugador1':
+            vidajugador2 = vida.jugador2 - 10
+            nodo.vida.maquina.innerHTML = vida.jugador1
+            
+            break;
+        case 'jugador2':
+            
+            vidaJugador1 = vida.jugador1 - 10
+            nodo.vida.maquina.innerHTML = vidaMaquina1
+            
+            break;
+       
+    }
+}
+
+
+
+class Player {
+    constructor(name, life, imagen){
+    this.name = name;
+    this.life = life;
+    this.imagen = imagen;
+}
+
+};
+let j1 = new Player('Incineroar', 100, "https://projectpokemon.org/images/sprites-models/homeimg/poke_capture_0727_000_mf_n_00000000_f_n.png") // personajes1
+let j2 = new Player('Pikachu', 100, "https://projectpokemon.org/images/sprites-models/homeimg/poke_capture_0025_001_mo_n_00000000_f_n.png") //personajes2
+let j3 = new Player('mewtwo', 100, "https://projectpokemon.org/images/sprites-models/homeimg/poke_capture_0150_000_uk_n_00000000_f_n.png")//personajes3
+let j4 = new Player('Greninja', 100, "https://projectpokemon.org/images/sprites-models/homeimg/poke_capture_0658_000_mf_n_00000000_f_n.png")//personaje4
+
+
+
+class Jugador {
+
+    constructor(player) {
+        this.personaje = player
+    }
+  
+}
+let jugador1 = new Jugador()
+console.log(jugador1) // jugador1.personaje = unfenid
+let jugador2 = new Jugador()
+console.log(jugador2) // jugador2.personaje = unfenid
+
+
+const seleccionarJugador = (player) => {
+
+
+    if (jugador1.personaje == undefined || jugador2.personaje == undefined) {
+
+
+        switch (player) {
+
+
+            case 'incineroar'://personaje1
+                if (jugador1.personaje == undefined) {
+
+                    jugador1.personaje = j1;
+                    console.log(jugador1.personaje)
+
+                } else if (jugador2.personaje == undefined && jugador1.personaje !== j1) {
+
+                    jugador2.personaje = j1;
+                   console.log(jugador2.personaje)
+                    
+                    escenarioLucha()
+                }
+                break
+            case 'pikachu'://personaje2
+                if (jugador1.personaje == undefined) {
+
+                    jugador1.personaje = j2;
+                    console.log(jugador1.personaje)
+
+                } else if (jugador2.personaje == undefined && jugador1.personaje !== j2) {
+                    jugador2.personaje = j2;
+                    console.log(jugador2.personaje)
+                    escenarioLucha()
+                }
+                break
+
+            case 'mewtwo'://personaje3
+                if (jugador1.personaje == undefined) {
+
+                    jugador1.personaje = j3;
+                    console.log(jugador1.personaje)
+
+                } else if (jugador2.personaje == undefined && jugador1.personaje !== j3) {
+                    
+                    jugador2.personaje = j3;
+                    console.log(jugador2.personaje)
+                    
+                    escenarioLucha()
+                }
+                break
+            case 'greninja'://personaje4
+                if (jugador1.personaje == undefined) {
+
+                    jugador1.personaje = j4;
+                    console.log(jugador1.personaje)
+
+                } else if (jugador2.personaje == undefined && jugador1.personaje !== j4) {
+
+                    jugador2.personaje = j4;
+                    console.log(jugador2.personaje)
+                    
+                    escenarioLucha()
+                }
+                break
+       
+            }
+           
+      } 
+}
+
+
+function escenarioLucha(){
+
+    seleccion.style.display= "none"
+    pelea.style.display= "flex"
+    innerHTML = jugador1.personaje.imagen
+    innerHTML = jugador2.personaje.imagen
+
+}
+
+
+
+
